6f39ea4fce7aa0011a6e8c94ce1c5feaabf92631
Blankycan <blankycan@gmail.com>
2023-03-25 19:04:34 +0100

    Build one large chunk mesh, still rendering all cube faces.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use bevy::prelude::*;
 use bevy::render::mesh::{Indices, Mesh};
 use bevy::render::render_resource::PrimitiveTopology;
+use bevy::window::PresentMode;
 
 #[macro_use]
 extern crate lazy_static;
@@ -17,11 +18,18 @@ use chunk::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                present_mode: PresentMode::Immediate,
+                ..default()
+            }),
+            ..default()
+        }))
         .add_plugin(FlyCameraPlugin)
         .add_plugin(DebugInfoPlugin)
         .add_startup_system(setup)
         .add_startup_system(create_chunk)
+        //.add_startup_system(create_chunk_by_individual_cubes)
         //.add_startup_system(setup_cube)
         //.add_startup_system(setup_triangle)
         //.add_system(update)
@@ -61,6 +69,21 @@ fn setup(mut commands: Commands) {
 }
 
 fn create_chunk(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let chunk = Chunk::new_random(0.8);
+
+    commands.spawn(MaterialMeshBundle {
+        mesh: meshes.add(create_chunk_mesh(&chunk).into()),
+        material: materials.add(Color::WHITE.into()),
+        //transform: Transform::from_xyz(x as f32, y as f32, z as f32),
+        ..default()
+    });
+}
+
+fn create_chunk_by_individual_cubes(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
@@ -123,6 +146,187 @@ fn setup_cube(
         Cube,
     ));
     */
+}
+
+fn create_chunk_mesh(chunk: &Chunk) -> Mesh {
+    const HALF_SIZE: f32 = 0.5;
+
+    let mut vertices = Vec::<([f32; 3], [f32; 3], [f32; 2])>::new();
+    let mut indices = Vec::<u32>::new();
+    let mut vert_index = 0;
+
+    for x in 0..CHUNK_SIZE {
+        for y in 0..CHUNK_SIZE {
+            for z in 0..CHUNK_SIZE {
+                let index: usize = Chunk::index_from(x, y, z);
+
+                if let Some(voxel) = chunk.get_voxel(index) {
+                    if !voxel.active {
+                        continue;
+                    }
+                    let pos = Vec3::new(x as f32, y as f32, z as f32);
+
+                    let cube_vertices = [
+                        // Front
+                        (
+                            [pos.x + HALF_SIZE, pos.y + HALF_SIZE, pos.z + HALF_SIZE],
+                            [0.0, 0.0, 1.0],
+                            [1.0, 0.0],
+                        ), // FTR
+                        (
+                            [pos.x - HALF_SIZE, pos.y + HALF_SIZE, pos.z + HALF_SIZE],
+                            [0.0, 0.0, 1.0],
+                            [0.0, 0.0],
+                        ), // FTL
+                        (
+                            [pos.x - HALF_SIZE, pos.y - HALF_SIZE, pos.z + HALF_SIZE],
+                            [0.0, 0.0, 1.0],
+                            [0.0, 1.0],
+                        ), // FBL
+                        (
+                            [pos.x + HALF_SIZE, pos.y - HALF_SIZE, pos.z + HALF_SIZE],
+                            [0.0, 0.0, 1.0],
+                            [1.0, 1.0],
+                        ), // FBR
+                        // Right
+                        (
+                            [pos.x + HALF_SIZE, pos.y + HALF_SIZE, pos.z - HALF_SIZE],
+                            [1.0, 0.0, 0.0],
+                            [1.0, 0.0],
+                        ), // BTR
+                        (
+                            [pos.x + HALF_SIZE, pos.y + HALF_SIZE, pos.z + HALF_SIZE],
+                            [1.0, 0.0, 0.0],
+                            [0.0, 0.0],
+                        ), // FTR
+                        (
+                            [pos.x + HALF_SIZE, pos.y - HALF_SIZE, pos.z + HALF_SIZE],
+                            [1.0, 0.0, 0.0],
+                            [0.0, 1.0],
+                        ), // FBR
+                        (
+                            [pos.x + HALF_SIZE, pos.y - HALF_SIZE, pos.z - HALF_SIZE],
+                            [1.0, 0.0, 0.0],
+                            [1.0, 1.0],
+                        ), // BBR
+                        // Back
+                        (
+                            [pos.x - HALF_SIZE, pos.y + HALF_SIZE, pos.z - HALF_SIZE],
+                            [0.0, 0.0, -1.0],
+                            [1.0, 0.0],
+                        ), // BTL
+                        (
+                            [pos.x + HALF_SIZE, pos.y + HALF_SIZE, pos.z - HALF_SIZE],
+                            [0.0, 0.0, -1.0],
+                            [0.0, 0.0],
+                        ), // BTR
+                        (
+                            [pos.x + HALF_SIZE, pos.y - HALF_SIZE, pos.z - HALF_SIZE],
+                            [0.0, 0.0, -1.0],
+                            [0.0, 1.0],
+                        ), // BBR
+                        (
+                            [pos.x - HALF_SIZE, pos.y - HALF_SIZE, pos.z - HALF_SIZE],
+                            [0.0, 0.0, -1.0],
+                            [1.0, 1.0],
+                        ), // BBL
+                        // Left
+                        (
+                            [pos.x - HALF_SIZE, pos.y + HALF_SIZE, pos.z + HALF_SIZE],
+                            [-1.0, 0.0, 0.0],
+                            [1.0, 0.0],
+                        ), // FTL
+                        (
+                            [pos.x - HALF_SIZE, pos.y + HALF_SIZE, pos.z - HALF_SIZE],
+                            [-1.0, 0.0, 0.0],
+                            [0.0, 0.0],
+                        ), // BTL
+                        (
+                            [pos.x - HALF_SIZE, pos.y - HALF_SIZE, pos.z - HALF_SIZE],
+                            [-1.0, 0.0, 0.0],
+                            [0.0, 1.0],
+                        ), // BBL
+                        (
+                            [pos.x - HALF_SIZE, pos.y - HALF_SIZE, pos.z + HALF_SIZE],
+                            [-1.0, 0.0, 0.0],
+                            [1.0, 1.0],
+                        ), // FBL
+                        // Top
+                        (
+                            [pos.x + HALF_SIZE, pos.y + HALF_SIZE, pos.z - HALF_SIZE],
+                            [0.0, 1.0, 0.0],
+                            [1.0, 0.0],
+                        ), // BTR
+                        (
+                            [pos.x - HALF_SIZE, pos.y + HALF_SIZE, pos.z - HALF_SIZE],
+                            [0.0, 1.0, 0.0],
+                            [0.0, 0.0],
+                        ), // BTL
+                        (
+                            [pos.x - HALF_SIZE, pos.y + HALF_SIZE, pos.z + HALF_SIZE],
+                            [0.0, 1.0, 0.0],
+                            [0.0, 1.0],
+                        ), // FTL
+                        (
+                            [pos.x + HALF_SIZE, pos.y + HALF_SIZE, pos.z + HALF_SIZE],
+                            [0.0, 1.0, 0.0],
+                            [1.0, 1.0],
+                        ), // FTR
+                        // Bottom
+                        (
+                            [pos.x - HALF_SIZE, pos.y - HALF_SIZE, pos.z - HALF_SIZE],
+                            [0.0, -1.0, 0.0],
+                            [1.0, 0.0],
+                        ), // BBL
+                        (
+                            [pos.x + HALF_SIZE, pos.y - HALF_SIZE, pos.z - HALF_SIZE],
+                            [0.0, -1.0, 0.0],
+                            [0.0, 0.0],
+                        ), // BBR
+                        (
+                            [pos.x + HALF_SIZE, pos.y - HALF_SIZE, pos.z + HALF_SIZE],
+                            [0.0, -1.0, 0.0],
+                            [0.0, 1.0],
+                        ), // FBR
+                        (
+                            [pos.x - HALF_SIZE, pos.y - HALF_SIZE, pos.z + HALF_SIZE],
+                            [0.0, -1.0, 0.0],
+                            [1.0, 1.0],
+                        ), // FBL
+                    ];
+                    vertices.extend(cube_vertices.iter().cloned());
+
+                    for _ in 0..6 {
+                        indices.push(vert_index);
+                        indices.push(vert_index + 1);
+                        indices.push(vert_index + 2);
+                        indices.push(vert_index);
+                        indices.push(vert_index + 2);
+                        indices.push(vert_index + 3);
+                        vert_index += 4;
+                    }
+                }
+            }
+        }
+    }
+
+    let mesh_indices = Indices::U32(indices);
+
+    let mut positions = Vec::new();
+    let mut normals = Vec::new();
+    let mut uvs = Vec::new();
+    for (position, normal, uv) in vertices.iter() {
+        positions.push(*position);
+        normals.push(*normal);
+        uvs.push(*uv);
+    }
+
+    let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
+    mesh.set_indices(Some(mesh_indices));
+    mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, positions);
+    mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
+    mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, uvs);
+    mesh
 }
 
 fn create_cube() -> Mesh {
