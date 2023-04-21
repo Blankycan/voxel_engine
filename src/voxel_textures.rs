49126406f9d648a689d91943c038c79457665eb8
Blankycan <blankycan@gmail.com>
2023-04-21 17:48:38 +0200

    Updated textures to be 4x4, with an outline around them to stop edge issues.

--- src/voxel_textures.rs
+++ src/voxel_textures.rs
@@ -4,7 +4,8 @@ use crate::{face::Side, face::Side::*, voxel::VoxelType, voxel::VoxelType::*};
 
 const TEXTURE_WIDTH: f32 = 512.0;
 const TEXTURE_HEIGHT: f32 = 512.0;
-const SPRITE_SIZE: f32 = 16.0;
+const SPRITE_SIZE: f32 = 4.0;
+const SPRITE_OFFSET: f32 = 1.0;
 
 pub fn get_voxel_type_uv(voxel_type: VoxelType, side: Side) -> [Vec2; 4] {
     match voxel_type {
@@ -20,22 +21,26 @@ pub fn get_voxel_type_uv(voxel_type: VoxelType, side: Side) -> [Vec2; 4] {
 }
 
 fn get_uv_for_index(x: usize, y: usize) -> [Vec2; 4] {
+    let x0: f32 = x as f32;
+    let y0: f32 = y as f32;
+    let x1: f32 = (x + 1) as f32;
+    let y1: f32 = (y + 1) as f32;
     [
         Vec2::new(
-            ((x + 1) as f32 * SPRITE_SIZE) / TEXTURE_WIDTH,
-            (y as f32 * SPRITE_SIZE) / TEXTURE_HEIGHT,
+            (x1 * SPRITE_SIZE + (x1 * 2.0 - 1.0) * SPRITE_OFFSET) / TEXTURE_WIDTH,
+            (y0 * SPRITE_SIZE + (y0 * 2.0 + 1.0) * SPRITE_OFFSET) / TEXTURE_HEIGHT,
         ),
         Vec2::new(
-            (x as f32 * SPRITE_SIZE) / TEXTURE_WIDTH,
-            (y as f32 * SPRITE_SIZE) / TEXTURE_HEIGHT,
+            (x0 * SPRITE_SIZE + (x0 * 2.0 + 1.0) * SPRITE_OFFSET) / TEXTURE_WIDTH,
+            (y0 * SPRITE_SIZE + (y0 * 2.0 + 1.0) * SPRITE_OFFSET) / TEXTURE_HEIGHT,
         ),
         Vec2::new(
-            (x as f32 * SPRITE_SIZE) / TEXTURE_WIDTH,
-            ((y + 1) as f32 * SPRITE_SIZE) / TEXTURE_HEIGHT,
+            (x0 * SPRITE_SIZE + (x0 * 2.0 + 1.0) * SPRITE_OFFSET) / TEXTURE_WIDTH,
+            (y1 * SPRITE_SIZE + (y1 * 2.0 - 1.0) * SPRITE_OFFSET) / TEXTURE_HEIGHT,
         ),
         Vec2::new(
-            ((x + 1) as f32 * SPRITE_SIZE) / TEXTURE_WIDTH,
-            ((y + 1) as f32 * SPRITE_SIZE) / TEXTURE_HEIGHT,
+            (x1 * SPRITE_SIZE + (x1 * 2.0 - 1.0) * SPRITE_OFFSET) / TEXTURE_WIDTH,
+            (y1 * SPRITE_SIZE + (y1 * 2.0 - 1.0) * SPRITE_OFFSET) / TEXTURE_HEIGHT,
         ),
     ]
 }
